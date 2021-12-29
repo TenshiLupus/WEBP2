863b71cd878bdc6ce93b5c8170b4df2d89967e93
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2021-12-29 02:45:16 +0100

    4.2 cookies set, page working

--- 4/4.2/example_2.php
+++ 4/4.2/example_2.php
@@ -1,5 +1,10 @@
 <?php
-    header("Content-type: text/html");
+    header("content-type: text/html");
+
+    echo nl2br($_COOKIE["session"] . "\n");
+    foreach($_GET as $item => $itemValue){
+        echo nl2br("$item= $itemValue" . "\n");
+    }
 
     
 
