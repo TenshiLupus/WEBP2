320477a811f2f96710a41fafd422c67cb2eeff6d
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2022-01-06 11:48:08 +0100

    database transaction, prepared statements

--- 6/6.1/databaseLog.php
+++ 6/6.1/databaseLog.php
@@ -1,4 +1,6 @@
 <?php
+header("content-type: text/html");
+
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=logs','root','');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -12,15 +14,21 @@ $currentDate = date('Y-m-d H:i:s');
 
 $query = $pdo->prepare("INSERT INTO users (Datetd, Clientaddr, Client) VALUES (:datetd, :clientAddr, :client)");
 $query->bindValue(':datetd', $currentDate);
-$query->bindValue(':clientaddr', $address);
+$query->bindValue(':clientAddr', $address);
 $query->bindValue(':client', $client);
 
 
 $query->execute();
-// $statement = $pdo->prepare('SELECT * FROM Users ORDER BY Tid DESC');
-// $statement->execute();
-// $logs = $statement->fetchAll(PDO::FETCH_ASSOC);
+$statement = $pdo->prepare('SELECT * FROM Users ORDER BY Datetd DESC');
+$statement->execute();
+$logs = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($logs as $item => $user){
+    echo "Tid: " . nl2br($user["Datetd"] . "\n");
+    echo "REMOTE_ADDR: " . nl2br($user["Clientaddr"] . "\n");
+    echo "REMOTE_USER_AGENT: " . nl2br($user["Client"] . "\n");
+    echo nl2br("\n");
+}
 
 $pdo = null;
-echo '<pre>';
-echo '<pre>';
+
