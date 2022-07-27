0617365714a229140bd3666b1a337c8d2be887bc
Tenshilupus <Angel__CM@hotmail.com>
2022-07-28 00:18:42 +0200

    authentication and verification of passwords.

--- 9/gesallprov/repository/users.js
+++ 9/gesallprov/repository/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+const scrypt = util.promisify(crypto.scrypt);
+
 
 class UsersRepository {
   constructor(filename) {
@@ -26,12 +29,29 @@ class UsersRepository {
   async create(attributes) {
     attributes.id = this.randomId();
 
+    const salt = crypto.randomBytes(8).toString('hex');
+    const buf = await scrypt(attributes.password, salt, 64);
+    
     const records = await this.getAll();
-    records.push(attributes);
+    const record = {
+      ...attributes,
+      password: `${buf.toString('hex')}.${salt}`
+    };
+    records.push(record);
 
     await this.writeAll(records);
 
-    return attributes;
+    return record;
+  }
+
+  async comparePasswords(saved, supplied){
+    // Save -> password saved in our database. 'hashed.salt'
+    // Supplied -> password given to us by a user trying to sign in.
+    const [hashedPassword, salt] = saved.split('.');
+    const hashedSuppliedBuffer = await scrypt(supplied, salt, 64);
+
+    return hashedPassword === hashedSuppliedBuffer.toString('hex');
+
   }
 
   async writeAll(records) {
