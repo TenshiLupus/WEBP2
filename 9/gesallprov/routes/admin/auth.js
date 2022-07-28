5370d57e63df100d71a27f191b39b2c521b7205d
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2022-07-28 13:05:24 +0200

    authentication validator installed

--- 9/gesallprov/routes/admin/auth.js
+++ 9/gesallprov/routes/admin/auth.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const usersRepo = require('../../repository/users.js');
+const signupTemplate = require('../../views/admin/authentication/signup');
+const signInTemplate = require('../../views/admin/authentication/signin');
+
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
-    res.send(`
-      <div>
-        Your id is: ${req.session.userId}
-        <form method="POST">
-          <input name="email" placeholder="email" />
-          <input name="password" placeholder="password" />
-          <input name="passwordConfirmation" placeholder="password confirmation" />
-          <button>Sign Up</button>
-        </form>
-      </div>
-    `);
+    res.send(signupTemplate({req}));
 });
 
 router.post('/signup', async (req, res) => {
@@ -43,15 +36,7 @@ router.get('/signout', (req, res) => {
 });
 
 router.get('/signin', (req, res) => {
-    res.send(`
-      <div>
-        <form method="POST">
-          <input name="email" placeholder="email" />
-          <input name="password" placeholder="password" />
-          <button>Sign In</button>
-        </form>
-      </div>
-    `);
+    res.send(signInTemplate());
 });
 
 router.post('/signin', async (req, res) => {
---
+++ 9/gesallprov/views/admin/layout.js
@@ -0,0 +1,15 @@
+
+
+module.exports = ( {content} ) =>{
+    return `
+        <DOCTYPE html>
+        <html lang="en" >
+            <head>
+
+            </head>
+            <body>
+                ${content}
+            </body>
+        </html>
+    `
+}
