d43283f122586516ea366bf04f2e2d0a2d5f3d85
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2022-08-07 12:33:04 +0200

    Revert "mongodb install"
    This reverts commit 0c317adab9262d971df3a3a247d28d27749bbdc8.

--- 9/gesallprov/index.js
+++ 9/gesallprov/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-
 //Possible application routes
 const authRouter = require('./routes/admin/auth');
 const adminProductsRouter = require('./routes/admin/products');
